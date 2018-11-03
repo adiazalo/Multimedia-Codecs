55ad204c7d31952dafa92dab3a5e49ac83d94ca2
Alfonso Diaz <adiazalo@protonmail.com>
2018-11-02 23:20:47 -0700

    Motion

--- Motion/motion_estimation.m
+++ Motion/motion_estimation.m
@@ -9,41 +9,52 @@ nBlks = (currFrRow*currFrCol)/(blkx*blky);
 mvx = zeros(currFrRow/blky,currFrCol/blkx);
 mvy = zeros(currFrRow/blky,currFrCol/blkx);
 
-n = 1;
+% n = 1;
 rIndex = 1;
 cIndex = 1;
 rWinIndex = 1;
 cWinIndex = 1;
-while cIndex<144
-    while rIndex<176
-        %extract block from curr
-        currBlk = curr(rIndex:rIndex+blky-1, cIndex:cIndex+blkx-1); 
-        prevBlk = prev(rIndex:rIndex+blky-1, cIndex:cIndex+blkx-1);
-        
-        n = 1;
-        while n <= nBlks
-            %check if window is in allowed space 
-            if rIndex-vRange >= 1 && rIndex-vRange+blky <= currFrRow
-                if cIndex-hRange >=1 && cIndex-hRange+blkx <= currFrCol
-                    
-                    window = prev(rIndex-vRange:rIndex-vRange+blky-1,cIndex-hRange:cIndex-hRange+blkx-1);
+% while cIndex<144
+%     while rIndex<176
+%         %extract block from curr
+%         currBlk = curr(rIndex:rIndex+blky-1, cIndex:cIndex+blkx-1); 
+%         prevBlk = prev(rIndex:rIndex+blky-1, cIndex:cIndex+blkx-1);
+%         
+%         n = 1;
+%         while n <= nBlks
+%             %check if window is in allowed space 
+%             if rIndex-vRange >= 1 && rIndex-vRange+blky <= currFrRow
+%                 if cIndex-hRange >=1 && cIndex-hRange+blkx <= currFrCol
+%                     
+%                     window = prev(rIndex-vRange:rIndex-vRange+blky-1,cIndex-hRange:cIndex-hRange+blkx-1);
+% 
+%                     % SAD
+%                     ab = abs(currBlk - window);
+%                     SAD_Matrix(n) = sum(ab(:));
+%                     n=n+1;
+%                     end
+%                 else
+%                     hRange = hRange - 1;
+%                 end
+%             else
+%                 vRange = vRange - 1;
+%         end
+%             
+% 
+%         end
+%         rIndex = rIndex + blky;
+%     end
+%     rIndex = 1;
+%     cIndex = cIndex + blkx;
+% end
 
-                    % SAD
-                    ab = abs(currBlk - window);
-                    SAD_Matrix(n) = sum(ab(:));
-                    n=n+1;
-                    end
-                else
-                    hRange = hRange - 1;
-                end
-            else
-                vRange = vRange - 1;
-        end
-            
 
-        end
-        rIndex = rIndex + blky;
-    end
-    rIndex = 1;
-    cIndex = cIndex + blkx;
-end
+while cWinIndex<144
+    while rWinIndex<176
+        if abs(rIndex-rWinIndex) <= search_range && abs(rIndex-rWinIndex) <= search_range
+            % window is in range
+            window = prev(rWinIndex:rWinIndex+blky-1,cWinIndex:cWinIndex+blkx-1);
+
+
+
+
